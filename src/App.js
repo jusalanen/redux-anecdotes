55cb2a29869cc4e22edde9c563eff8a4b9970197
Jussi Alanen <jussi.alanen@fonet.fi>
2018-12-22 15:46:01 +0200

    aloittaa Osan 6

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Notification from './components/Notification'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
-class App extends Component {
+class App extends React.Component {
+
   render() {
+    const anecdotes = this.props.store.getState()
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Programming anecdotes</h1>
+        <Notification />
+        <AnecdoteList store={this.props.store} />
+        <AnecdoteForm store={this.props.store} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
